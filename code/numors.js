05d3b426a1c7eb6cb7a13eb0cc29d4882c6cbf67
coder-sam-007 <sameerjain.coder@gmail.com>
2023-02-26 11:35:25 +0530

    framework commented

--- code/numors.js
+++ code/numors.js
@@ -2,18 +2,28 @@ const nav_items = document.querySelectorAll(".nav-item");
 const section = document.querySelector("[section='content']");
 const sec_attr = section.getAttribute("section-id");
 
-const check = (li) => {
-  if (li.classList.contains("active-page")) li.classList.remove("active-page");
-};
+// const check = (li) => {
+//   if (li.classList.contains("active-page")) li.classList.remove("active-page");
+// };
 
-nav_items.forEach((i) => {
-  i.addEventListener("click", () => {
-    nav_items.forEach(check);
-    i.classList.add("active-page");
-  });
-});
+// nav_items.forEach((i) => {
+//   i.addEventListener("click", () => {
+//     nav_items.forEach(check);
+//     i.classList.add("active-page");
+//   });
+// });
 
 //*****   code for auto detection ******//
+
+// for home page
+
+if (sec_attr == "h") {
+  nav_items.forEach((i) => {
+    if (i.querySelector("a>img").alt == "Home") {
+      i.classList.add("active-page");
+    }
+  });
+}
 
 // for explore page
 
@@ -31,6 +41,26 @@ if (sec_attr == "e") {
 if (sec_attr == "m") {
   nav_items.forEach((i) => {
     if (i.querySelector("a>img").alt == "Marketplace") {
+      i.classList.add("active-page");
+    }
+  });
+}
+
+// for account page
+
+if (sec_attr == "a") {
+  nav_items.forEach((i) => {
+    if (i.querySelector("a>img").alt == "Account") {
+      i.classList.add("active-page");
+    }
+  });
+}
+
+// learning page
+
+if (sec_attr == "l") {
+  nav_items.forEach((i) => {
+    if (i.querySelector("a>img").alt == "Learning") {
       i.classList.add("active-page");
     }
   });
