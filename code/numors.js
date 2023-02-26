eac6013212ad9e930765627bf021b77475c88f4f
coder-sam-007 <sameerjain.coder@gmail.com>
2023-02-26 10:49:47 +0530

    event listeners added

--- code/numors.js
+++ code/numors.js
@@ -1,3 +1,14 @@
 const nav_items = document.querySelectorAll(".nav-item");
 const section = document.querySelector("[section='content']");
 const sec_attr = section.getAttribute("section-id");
+
+const check = (li) => {
+  if (li.classList.contains("active-page")) li.classList.remove("active-page");
+};
+
+nav_items.forEach((i) => {
+  i.addEventListener("click", () => {
+    nav_items.forEach(check);
+    i.classList.add("active-page");
+  });
+});
